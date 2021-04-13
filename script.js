f9b362461488a1557450968b67f0b1d7a3edf245
AgneseBettio <bettio.agnese@gmail.com>
2021-04-14 00:19:51 +0200

    aggiustare lettura uppercase

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ console.log(userSurname);
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 console.log(surnameList);
-
-switch (userSurname) {
-    case "Bianchi":
-    case "Neri":
-    case "Rossi":
-    case "Verdi":
-    case "Gialli":
-        alert("il cognome da lei inserito risulta già nel nostro sistema")
-        break;
-    default: surnameList.push(userSurname);
-    console.log(surnameList);
-}
-// 3. stampa la lista ordinata alfabeticamente
-surnameList.sort();
+surnameList.push(userSurname);
 console.log(surnameList);
 
+
+// 3. stampa la lista ordinata alfabeticamente
+var sortedSurnameList = surnameList.sort();
+console.log(sortedSurnameList);
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-var userPosition = surnameList.indexOf( userSurname) + 1;
+var printedList = document.getElementById("printed-list");
+for (var i = 0; i < sortedSurnameList.length; i++) {
+    var checkSurname = sortedSurnameList[i];
+    printedList.innerHTML += "<ol>" + sortedSurnameList[i] + "</ol>";
+}
+
+var userPosition = (SortedSurnameList.indexOf(userSurname)) - 1;
 console.log(userPosition);
+document.getElementById("user-position").innerHTML = "la tua posizione nella lista d'attesa è " + userPosition;
