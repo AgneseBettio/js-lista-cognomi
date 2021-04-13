5b2a9aca23b2de3977a484673f193ff4d49310cc
AgneseBettio <bettio.agnese@gmail.com>
2021-04-13 17:26:03 +0200

    secondo push

--- script.js
+++ script.js
@@ -12,16 +12,15 @@ switch (userSurname) {
     case "Rossi":
     case "Verdi":
     case "Gialli":
-        alert("il cognome da lei inserito risulta già nel sistema")
+        alert("il cognome da lei inserito risulta già nel nostro sistema")
         break;
     default: surnameList.push(userSurname);
     console.log(surnameList);
 }
-
 // 3. stampa la lista ordinata alfabeticamente
 surnameList.sort();
 console.log(surnameList);
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
-var surnamePosition [];
+var userPosition = surnameList.indexOf( userSurname) + 1;
+console.log(userPosition);
